149706bfdc5450e9c7d3cc8f96272503ef9b2c7a
yasmine <yasminekeskes08@gmail.com>
2020-04-11 22:12:10 +0200

    creating new Class ( Personnel )

---
+++ DAOpattern/src/main/java/Prog/gl/DAOpattern/Personnel.java
@@ -0,0 +1,60 @@
+package Prog.gl.DAOpattern;
+
+import java.time.LocalDate;
+
+public class Personnel implements Serializable{
+	//Attributs
+	private String nom;
+	private String prenom;
+	private LocalDate dateNaissance;
+	private String numeroTel;
+	private String fonction;
+	
+	//Constructor
+	public Personnel() {
+		
+	}
+	
+	
+	//Getters & setters
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public LocalDate getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(LocalDate dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public String getNumeroTel() {
+		return numeroTel;
+	}
+	public void setNumeroTel(String numeroTel) {
+		this.numeroTel = numeroTel;
+	}
+	public String getFonction() {
+		return fonction;
+	}
+	public void setFonction(String fonction) {
+		this.fonction = fonction;
+	}
+	
+	//methods
+	
+	public void create() {
+		
+	}
+	
+	
+
+}
